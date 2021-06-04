94d8aaa182a6e1d637b1624c25a5b3c5dc965b3b
Hassan Dufer <hadufer@c1r2p7.42nice.fr>
2021-06-04 12:58:44 +0200

    Modif on ex09

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: hadufer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 12:22:16 by hadufer           #+#    #+#             */
-/*   Updated: 2021/06/04 12:28:44 by hadufer          ###   ########.fr       */
+/*   Updated: 2021/06/04 12:56:42 by hadufer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 int	ft_str_is_alpha(char *str)
@@ -52,11 +52,21 @@ char	*ft_strcapitalize(char *str)
 
 	i = 0;
 	new_word = 0;
+	if (!str || *str = 0)
+			return str;
 	while (str[i])
 	{
-		if(new_word == 1 && ft_str_is_numeric(&(str + i)) && ft_str_is_alpha(&(str + i)))
+	// FAIRE TOUTE CETTE MERDE A LA MAIN
+		if(new_word == 1 && ft_str_is_numeric((str + i)) && ft_str_is_alpha((str + i)))
 		{
-			
+			new_word = 0;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+			else if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] += 32;
 		}
+		else
+			new_word = 1;
+		i++;
 	}
 }
---
+++ c02/ex09/main.c
@@ -0,0 +1,6 @@
+#include "ft_strcapitalize.c"
+
+int main()
+{
+	printf("%s\n", ft_strcapitalize("hello my 42old friend"));
+}
