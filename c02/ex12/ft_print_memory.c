cf218fe035b8eb07ef86ed09974b5a2b1bde25c2
hdufer <hdufer@student.42.fr>
2021-06-06 13:56:57 +0200

    Totally working but 1 byte is skipped when switching block

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -1,41 +1,91 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_print_memory.c                                  :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hadufer <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/05 17:06:52 by hadufer           #+#    #+#             */
-/*   Updated: 2021/06/05 17:54:50 by hadufer          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-void	ft_putstr(char *str)
-{
-	unsigned int	i;
+#include <stdio.h>
 
-	i = 0;
-	while (str[i])
-		i++;
-	write(1, str, i);
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
-unsigned char	read_byte(void *addr, int n)
+void	write_byte_address_format(unsigned long addr, int depth)
 {
-	unsigned char byte;
+	if (addr == 0)
+	{
+		while ((16 - depth) > 0)
+		{
+			ft_putchar('0');
+			depth++;
+		}
+		return ;
+	}
+	write_byte_address_format((addr / 16), depth + 1);
+	if ((addr % 16) < 10)
+		ft_putchar(addr % 16 + '0');
+	else
+		ft_putchar(((addr % 16) - 10) + 'a');
+}
 
-	byte = 0;
-	byte = (unsigned char)(addr + n);
-	return (byte);
+void	write_byte_hex(unsigned char byte)
+{
+	if (byte == 0)
+		return ;
+	write_byte_hex(byte / 16);
+	if ((byte % 16) < 10)
+		ft_putchar(byte % 16 + '0');
+	else
+		ft_putchar(((byte % 16) - 10) + 'a');
 }
 
 void	*ft_print_memory(void *addr, unsigned int size)
 {
-//	unsigned char	byte;
+	unsigned char	*beg;
+	unsigned char	*byte;
+	unsigned int	i;
+	unsigned int	k;
 
-//	byte = (unsigned char)(addr + n);
-//	while ()
-	ft_putstr(read_byte);
+	i = 0;
+	byte = (unsigned char*)(addr);
+	if (size <= 0)
+		return (addr);
+	while (i < size)
+	{
+		write_byte_address_format((size_t)byte, 0);
+		ft_putchar(':');
+		ft_putchar(' ');
+		while ((i < size) && ((i+1) % 17))
+		{
+			if (i >= 17 && ((i % 16) == 1))
+			{
+				write_byte_hex(*(byte + (i - 1)));
+			}
+			if (i % 16 == 0)
+				beg = (addr + i);
+			byte = (addr + i);
+			if (*byte)
+				write_byte_hex(*byte);
+			else
+			{
+				ft_putchar('0');
+				ft_putchar('0');
+			}
+			if ((i > 16) && (i % 2 == 0))
+				ft_putchar(' ');
+			else if ((i < 17) && (i+1) % 2 == 0)
+				ft_putchar(' ');
+			i++;
+		}
+		if (i > 17 && i % 2 == 0)
+			ft_putchar(' ');
+		k = 0;
+		while (beg <= byte)
+		{
+			if (*beg >= 0 && *beg <= 31)
+				ft_putchar('.');
+			if (*beg >= 32 && *beg <= 127)
+				ft_putchar(*beg);
+			beg++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
 }
--- c02/ex12/main.c
+++ c02/ex12/main.c
@@ -1,8 +1,10 @@
+#include <stdio.h>
+
 void	ft_print_memory(void *addr, unsigned int size);
 
 int	main(void)
 {
 	char *test;
-	asprintf(&test,"abc\0");
-	ft_print_memory(test, 4);
+	asprintf(&test,"abcdefghijklmnopqrstuvwxyz");
+	ft_print_memory(test, 26);
 }
