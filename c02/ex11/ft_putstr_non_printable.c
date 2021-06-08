b51865ef6d87bb6357eb2c7cb22932f9d20e38a7
hdufer <hdufer@student.42.fr>
2021-06-08 22:42:08 +0200

    Instead of printing non printable in if i moved it in else let see if it work | ft_putstr removed.

--- c02/ex11/ft_putstr_non_printable.c
+++ c02/ex11/ft_putstr_non_printable.c
@@ -3,27 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hadufer <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: hadufer <hadufer@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 12:26:34 by hadufer           #+#    #+#             */
-/*   Updated: 2021/06/08 13:18:44 by hadufer          ###   ########.fr       */
+/*   Updated: 2021/06/08 22:41:58 by hadufer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	long	length;
-
-	length = 0;
-	while (str[length])
-		length++;
-	write(1, str, length);
 }
 
 void	ft_putchar_hex(char c)
@@ -44,15 +35,15 @@ void	ft_putstr_non_printable(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] >= 1 && str[i] <= 31)
+		if (str[i] >= 32 && str[i] <= 127)
+			ft_putchar(str[i]);
+		else
 		{
 			ft_putchar('\\');
 			if (str[i] < 16)
 				ft_putchar('0');
 			ft_putchar_hex(str[i]);
 		}
-		else
-			ft_putchar(str[i]);
 		i++;
 	}
 }
