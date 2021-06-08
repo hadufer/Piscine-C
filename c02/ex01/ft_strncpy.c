45006cd26a286406b2263b9afd43b53f0f3359ff
hdufer <hdufer@student.42.fr>
2021-06-08 22:05:52 +0200

    function strncpy.c done ex01

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -3,45 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hadufer <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: hadufer <hadufer@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 09:49:24 by hadufer           #+#    #+#             */
-/*   Updated: 2021/06/08 19:00:11 by hadufer          ###   ########.fr       */
+/*   Updated: 2021/06/08 22:04:50 by hadufer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
-{
-	int	length;
-
-	length = 0;
-	while (str[length])
-		length++;
-	return (length);
-}
-
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int	i;
-	int	remainder;
+	unsigned int	i;
 
-	remainder = ft_strlen(dest) - ft_strlen(src);
 	i = 0;
-	while (i < n)
+	while (i < n && src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	if (ft_strlen(src) < n)
+	while (i < n)
 	{
-		if (remainder < 0)
-			return (dest);
-		while (remainder)
-		{
-			dest[i] = 0;
-			i++;
-			remainder--;
-		}
+		dest[i] = 0;
+		i++;
 	}
 	return (dest);
 }
