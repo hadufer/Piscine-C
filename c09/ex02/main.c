43c3732d2adc5c105432edf2c01f3b09c4869625
Hassan Dufer <hadufer@c1r2p7.42nice.fr>
2021-06-21 20:48:52 +0200

    push c09

---
+++ c09/ex02/main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hadufer <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/17 09:19:37 by hadufer           #+#    #+#             */
+/*   Updated: 2021/06/21 20:47:29 by hadufer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+char	**ft_split(char *str, char *charset);
+
+int	main()
+{
+	int	i;
+	char	*str = "sadasdasd s sda ds n";
+	char	*charset = "";
+
+	i = 0;
+	char **tab = ft_split(str, charset);
+	while (tab[i])
+	{
+		printf("%s\n", tab[i]);
+		i++;
+	}
+	free(tab);
+}
