2087da1c52562e553e7aa018afe88216076b0440
Hassan Dufer <hadufer@c1r2p7.42nice.fr>
2021-06-24 18:39:32 +0200

    c01 patch

--- c11/ex01/ft_map.c
+++ c11/ex01/ft_map.c
@@ -6,19 +6,25 @@
 /*   By: hadufer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 11:33:53 by hadufer           #+#    #+#             */
-/*   Updated: 2021/06/24 16:40:13 by hadufer          ###   ########.fr       */
+/*   Updated: 2021/06/24 18:38:10 by hadufer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
 
 int	*ft_map(int *tab, int length, int(*f)(int))
 {
 	int	i;
+	int	*ret;
 
+	ret = malloc(sizeof(int) * (length + 1));
+	if (!ret)
+		return (NULL);
 	i = 0;
 	while (i < length)
 	{
-		f(tab[i]);
+		ret[i] = f(tab[i]);
 		i++;
 	}
-	return (tab);
+	return (ret);
 }
